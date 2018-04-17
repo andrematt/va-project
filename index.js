ae48619b729a1ccf9f5c9754048db9ed12ed1063
unknown <andrea.num56@gmail.com>
2018-04-17 12:14:37 +0200

    timeFilter

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/*  
+problema: individuare dei pattern insoliti in un dataset di spostamenti
+
+suddivisibile in:
+1 ottenere dalla mappa un insieme di nodes (coordinate e tipo di luogo) usabili come base per la visualizzazione
+2 leggere i dati relativi agli spostamenti e mapparli ai punti
+		-inserire dei filtri sui veicoli(tutti o ranger, n di assi) e raggruppamenti (spostamenti in un giorno, settimana, mese)
+3 aggiungere delle visualizzazioni semplici relativi alle stats delle selezioni precedenti:
+		-grafico di lunghezza dei percorsi
+		-nelle tooltip, stats di tipo di veicoli che attraversano quel gate
+*/
+
 let dataStore=[];
 let svg;
 
@@ -19,8 +31,7 @@ function fetchData(){
 
 
 function initializeViz(data){
-	let parsedRoutes=parse(data[0]);
-  dataStore.push(parsedRoutes);
+  dataStore.push(data[0]);
   dataStore.push(data[1]);
 	const h=982;
 	const w=982;
@@ -30,7 +41,7 @@ function initializeViz(data){
     .attr("class", "tooltip")				
     .style("opacity", 0);
 	
-	svg = d3.select("#viz") //tiene la selezione per poterlo riusare dopo
+	svg = d3.select("#viz") //salva svg in global per poterlo riusare dopo
         .append("svg")
         .attr("class", "svg-container")
  				.attr("width", w)
@@ -44,18 +55,18 @@ function initializeViz(data){
 		.attr("stroke", "black")
 		.attr("r", 7)
 		.on("mouseover", function(d) {		
-        div.transition()		
-        .duration(200)		
-        .style("opacity", .9);		
-     		div .html(d.type +" "+d.value)
-     		.style("left", (d3.event.pageX) + "px")		
-         .style("top", (d3.event.pageY - 28) + "px");		
-          })					
-        .on("mouseout", function(d) {		
-            div.transition()		
-                .duration(500)		
-                .style("opacity", 0);	
-        });
+      div.transition() //si può fare classe css della transition?		
+          .duration(200)		
+          .style("opacity", .9);		
+     	div .html(d.type +" "+d.value)
+     		  .style("left", (d3.event.pageX) + "px")		
+          .style("top", (d3.event.pageY - 28) + "px");		
+     })					
+    .on("mouseout", function(d) {		
+      div.transition()		
+         .duration(500)		
+         .style("opacity", 0);	
+     });
 
 	svg.selectAll("circle")
 	.attr("cx", function(d,i){
@@ -86,25 +97,20 @@ function initializeViz(data){
 	});
 }
 
-
-function getPointCoords(data){
-	return[];
-}
-
 function parse(data){
-	let splitted=data.split(/,|\r\n/);
+	let splitted=data.split(/,|\r\n/); //divide a ogni "," o a nuova linea
 	let cloned=[];
-  for (i = 0; i < splitted.length; i=i+4) {
+  for (i = 0; i < splitted.length; i=i+4) { //ricostruisce la row originaria del csv
     cloned.push(splitted.slice(i, i+4));
   }
  	return(cloned);
 }	
 
 
-function vehicleFilter(){
+function vehicleFilter(data){
 	let e = document.getElementById("vehicleTypeList");
 	let type = e.options[e.selectedIndex].value;
-	let onlyVehicleType=dataStore[0].filter(element => element[2]===type);
+	let onlyVehicleType=data.filter(element => element[2]===type);
 	let sortedVehicles=onlyVehicleType.sort(function(a,b){ //ordina l'array in base all'id del veicolo
 		if (a[1] < b[1]) {
     	return -1;
@@ -125,7 +131,7 @@ function vehicleFilter(){
 	timeStamp.path=sortedVehicles[0][3];
 	singlePath.path.push(timeStamp);
 	for (var i=1; i<sortedVehicles.length;i++){
-		if (sortedVehicles[i][1]!==sortedVehicles[i-1][1] || (singlePath.path.length>1&&((sortedVehicles[i][3].startsWith('entrance')&&sortedVehicles[i-1][3].startsWith('entrance'))))){ //condizioni di terminazione: il veicolo seguente ha targa diversa oppure c'è un path di lunghezza >2 con due ingressi consecutivi
+		if (sortedVehicles[i][1]!==sortedVehicles[i-1][1] || (singlePath.path.length>1&&((sortedVehicles[i][3].startsWith('entrance')&&sortedVehicles[i-1][3].startsWith('entrance'))))){ //ARGH :D condizioni di terminazione: il veicolo seguente ha targa diversa oppure c'è un path di lunghezza >1 con due ingressi consecutivi
 			arrayPath.push(singlePath);
 			singlePath=new Object();
 			singlePath.path=[];
@@ -140,37 +146,50 @@ function vehicleFilter(){
 			arrayPath.push(singlePath);
 		}
 	}
-
-	drawPath(arrayPath);
+	console.log(arrayPath);
+	return arrayPath;
 }
 
 function drawPath(data){
-	console.log("initial data:");
-	console.log(data);
-  console.log("extracting the path:");
+	actualVized=data;
+	let div = d3.select("#viz").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+  //console.log("extracting the path:");
   let start =pathExtractor(data);
-  console.log(start);
 	let edges=svg.selectAll("polyline")
 		.data(start);
-	
-edges.exit().remove(); //rimuove la selezione precedente
 
-	console.log("edges obj:");
-	console.log(edges);
+	edges.exit().remove(); //rimuove la selezione precedente
+	
+	edges.attr("points", function(d, i){ //update: con merge non serve https://stackoverflow.com/questions/20708442/d3-js-chart-not-updated-with-new-data
+  	return d;
+	});  
 
 	edges.enter()
 		.append("polyline")
 		.attr("fill", "none")
 		.attr("stroke", "black")
-    .attr("stroke-width", 2)
-    .merge(edges)
+    .attr("stroke-width", 0.1)
+    //.merge(edges) //senza update nè merge i dati sono aggiunti solo da enter: aggiunge paths se più lunghi, ma non aggiorna quelli esistenti
     .attr("points", function(d, i){
-    	console.log(d);
     	return d;
-    });  
-
-   
-
+  	})
+  	.on("mouseover", function(d, i) {		
+  		console.log(d);
+  		console.log(i);
+      div.transition()
+          .duration(200)		
+          .style("opacity", .9);		
+     	div .html("Path tooltip!!!")
+     		  .style("left", (d3.event.pageX) + "px")		
+          .style("top", (d3.event.pageY - 28) + "px");		
+     })					
+    .on("mouseout", function(d) {		
+      div.transition()		
+         .duration(500)		
+         .style("opacity", 0);	
+     });  
 }
 
 function pathExtractor(data){
@@ -193,17 +212,47 @@ function getCoord(pointToFind){ //cerca in dataStore[1] le coord di data.path
 	return(onlySearchedPoint[0].coord);
 }
 
-/*  
-problema: individuare dei pattern insoliti in un dataset di spostamenti
+function draw(){
+	let parsedRoutes=parse(dataStore[0]); 
+	let timeFiltered=timeFilter(parsedRoutes);
+	if (timeFiltered.length>0) {
+		let pathArray=vehicleFilter(timeFiltered);
+		drawPath(pathArray);
+	}
+	else {
+		alert("no data to viz!")
+	}
+}
 
-suddivisibile in:
-1 ottenere dalla mappa un insieme di punti (coordinate e tipo) usabili come base per la visualizzazione
-2 leggere i dati relativi agli spostamenti e mapparli ai punti
-		-inserire dei filtri sui veicoli(tutti o ranger, n di assi) e raggruppamenti (spostamenti in un giorno, settimana, mese)
-3 aggiungere delle visualizzazioni semplici (barcharts) relativi alle stats delle selezioni precedenti
+function timeFilter(data){
+	let m = document.getElementById("monthList");
+	let mValue = m.options[m.selectedIndex].value;
+	let y = document.getElementById("yearList");
+	let yValue = y.options[y.selectedIndex].value;
+	let yearFiltered=[];
+	let monthFiltered=[];
+	if (yValue!=='0') {
+		yearFiltered=data.filter(function (d){
+			let sliced=d[0].slice(0,4);
+			if (sliced===yValue){
+				return d;
+			}	
+		});
+	}
+	else {
+		yearFiltered=data;
+	}
+	if (mValue!=='0') {
+		monthFiltered=yearFiltered.filter(function (d){
+			let sliced=d[0].slice(5,7);
+			if (sliced===mValue){
+				return d;
+			}	
+		});
+	}
+	else {
+		monthFiltered=yearFiltered;
+	}
+	return monthFiltered;
+}
 
-
-
-
-
-*/
