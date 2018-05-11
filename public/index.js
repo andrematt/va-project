53cef92465986c24bcd6d52ee53c0e541207baa7
andrematt <andrea.num56@gmail.com>
2018-05-12 01:11:01 +0200

    test

--- public/index.js
+++ public/index.js
@@ -1,59 +1,188 @@
-/*
 
 
- //map reduce process: map è una specie di pipeline, usata spesso con reduce: si mappa un set di data in uno (o più) values.
- //reduce functions: sono sia in d3, sia esistono in d3. d3min, max, sum, extends.. sono fatte trmaite reduce. Es: sommatoria dei valor:
-  //sum = aggregated from previous steps
-  let initialValue=0;
-  let totalPathLength = actualData.reduce(function(sum, d) {return sum+d.path.length}, initialValue);
-
-	nest: array di raggruppamenti
-	.rollup: riduzione di tutti i valori in function(d): prima si raggruppa, poi per ogni content di "values" si può passare il content alla funct rollup
-	per es trovare il min di tutti, o la media
-
-	
-	precompilare le operazioni stile olep per rendere le operazioni più veloci: crossfilter (linked display)
-	lo fa muovendo la complessità da tempo a spazio: fa tutto prima e salva in memoria
-	nella solita var che contiene crossfilter si possono accumulare vari filtraggi/grouping (es dimension raggruppa per una dimensione) del db
-
-	usare per fare un barchart scrollabile di traffico per data (traffico totale per lun, etc) e sotto raggruppamenti per mese/anno
-
-*/
-
-
-
-
-/*
-problema: individuare dei pattern insoliti in un dataset di spostamenti
-
-suddivisibile in:
-1 ottenere dalla mappa un insieme di nodes (coordinate e tipo di luogo) usabili come base per la visualizzazione
-2 leggere i dati relativi agli spostamenti e mapparli ai punti
-		-inserire dei filtri sui veicoli(tutti o ranger, n di assi) e raggruppamenti (spostamenti in un giorno, settimana, mese)
-3 aggiungere delle visualizzazioni semplici relativi alle stats delle selezioni precedenti:
-		-grafico di lunghezza dei percorsi
-		-nelle tooltip, stats sul tipo di veicoli che attraversano quel gate
-		-max e min path length per ogni tipo di veicolo
-
-TODO: 
-- box in alto con filtri attivi
-- possibilità di non filtrare per n assi
-- visualizzazione per singolo punto sulla mappa, stat di chi lo attaversa
-es https://hpbl.github.io/VAST-MC1/visualizations.html
-
-*/
-
-let dataStore=[]; //dati letti da file
+let dataStore; //dati letti da file
 let actualData=[]; //dati in uso per la visualizzazione
 let mappedCoord=[]; //mapping di coordinate a dimenzioni svg
+let pathData=[]; //array di paths non filtrati per essere usato con crossfitler
 let svg;
 let viewType=0;  //0 = graph, 1 = path length, 2 = path crossing
 const padding=80;
-	var h=800; //dimenzioni di default dell'svg, possono cambiare
-	var w=800;
+	var h=400; //dimenzioni di default dell'svg, possono cambiare
+	var w=400;
 
+
+
+
+function testCallback(){
+	const urlDB = 'http://localhost:3000/sensors';
+	let allPoints=fetch(urlDB).then((resp) => resp.json());
+	allPoints.then(function(value) {
+ 		console.log(value);
+ 	});
+
+	/* 	
+ 	let query = 'http://localhost:3000/sensor/entrance0';
+	let singlePoint=fetch(query).then((resp) => resp.json());
+	singlePoint.then(function(value) {
+ 		console.log(value);
+ 	})
+ 	*/
+}
+
+function pointStats(data){
+
+}
+
+function getPointData(gate){
+	let query = 'http://localhost:3000/sensor/'+gate;
+	console.log(query);
+	let result=[];
+	let singlePoint=fetch(query).then((resp) => resp.json());
+	singlePoint.then(function(value) {
+		value.forEach(function(val){
+			result.push(val);
+		})
+ 		pointStats(pathData);
+ 	  	//var xf = crossfilter(pathData);
+	    //console.log(xf.size());	
+	    //let test=nameFilter(name, pathData);
+	    //console.log(test);
+ 	});
+ 	
+		//estrai il nome del punto
+		//da dataStore estrai tutti gli attraversamenti di quel punto (filtra actualData con quel nome)
+		//non da actualData! qua i filtri "normali" vengono disattivati, e lavora crossfilter
+		//passa a crossfilter i dati filtrati
+		//costruisci raggruppamenti del traffico per giorno/mese/anno
+ 	
+}
 
 function fetchData(){
+	const urlPoints="../data/points.csv";
+	let pointData=fetch(urlPoints).then((resp) => resp.json());
+	pointData.then(function(values){
+		if(values){
+			dataStore=values;
+			initializeViz();
+			initializeNodes();
+			addOnclick();
+		}
+		else {
+			throw new Error("error fetching data!");
+		}
+	})
+}
+
+
+function pathStats(data){
+	
+	let h = 250;
+	let w = 600;
+	let maxX=d3.max(data, function(d){return d.pathLength});
+	let yValues=d3.nest()
+  		.key(function(d) { return d.pathLength;})
+   		.rollup(function(v) { return v.length; })
+		.entries(data);
+  	let maxY=d3.max(yValues, function(d){return d.value});
+  	let pixPerUnit = h / Math.log(maxY);
+   
+
+	let paths  = crossfilter(data);	
+	  all = paths.groupAll();
+      id = paths.dimension(function(d) { return d.vehicleId; }),
+      ids = id.group(),
+      path =paths.dimension(function(d) { return d.path; }),
+      pathLength = paths.dimension(function(d){ return d.pathLength; }),
+      lengths=pathLength.group();
+      type = paths.dimension(function(d) { return +d.vehicleType; }),
+      types = type.group();
+       topTypes = types.top(1);
+topTypes[0].key; // the top payment type (e.g., "tab")
+topTypes[0].value; // the count of payments of that type (e.g., 8)
+    
+    let lengthsChart = dc.barChart("#dc-lengths-chart");
+
+    lengthsChart.width(w)
+     .height(h)
+     .margins({top: 10, right: 10, bottom: 20, left: 40})
+     .dimension(pathLength)								// the values across the x axis
+     .group(lengths)							// the values on the y axis
+	 .transitionDuration(0)
+     .centerBar(true)	
+	 .y(d3.scaleLog().domain([1,maxY]))                               // bar width Keep increasing to get right then back off.
+     .x(d3.scaleLinear().domain([0, maxX+2]))
+	 .xAxis().tickFormat(function(v) {return v;});
+
+	 lengthsChart.on("renderlet", function(chart){ //dc non supporta le scale log (https://github.com/dc-js/dc.js/issues/477): hack orrendo per modificarle le barre dopo che sono state renderizzate
+    	chart.selectAll("g .bar")		
+            .attr("y", function(d) {
+             	if (d.y === 1) {
+					return(h - pixPerUnit);
+              	}
+              	else if (d.y===0){ //0 darebbe infinity: restituisco come valore di partenza la base del grafico
+                	return h; 	
+              	}
+			  	else {
+                	return (h - 1) - (pixPerUnit*Math.log(d.y));
+               		}	
+                })
+            .attr("height", function(d) {
+ 				return h;
+            });
+		});
+
+	  let vehiclesChart = dc.rowChart("#dc-vehicles-chart");
+
+      vehiclesChart.width(w)
+    .height(h)
+    .margins({top: 10, right: 10, bottom: 20, left: 40})
+    .dimension(type)								// the values across the x axis
+    .group(types)
+    .ordinalColors(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628'])							// the values on the y axis
+	.transitionDuration(500)
+	.label(function (d){
+      	if (d.key===1){
+      		return "1 axis";
+      	}
+      	else if (d.key==2){
+      		return "2 axis";
+      	}
+      	else if (d.key==3){
+      		return "2 axis ranger";
+      	}
+      	else if (d.key==4){
+      		return "3 axis";
+      	}
+      	else if (d.key==5){
+      		return "4 axis";
+      	}
+      	else if (d.key==6){
+      		return "5 axis";
+      	}
+    })
+    .elasticX(true);
+
+
+
+   dc.renderAll();
+}
+
+function loadAll(){
+	let query = 'http://localhost:3000/sensors/';
+	let result=[];
+	let allPoints=fetch(query).then((resp) => resp.json());
+	allPoints.then(function(value) {
+		value.forEach(function(val){
+			result.push(val);
+		})
+		let sorted=sortById(result);
+		let pathData=makePath(sorted);
+		pathStats(pathData);
+ 	  	//
+ 	});	
+}
+
+function fetchData2(){
 	const urlSensor="../data/sensor.csv";
 	const urlPoints="../data/points.csv";
 	let sensorData=fetch(urlSensor).then((resp) => resp.text());
@@ -62,6 +191,7 @@ function fetchData(){
 		if (values[0]&&values[1]) {
   		dataStore.push(values[0]);
   		dataStore.push(values[1]);
+  		prepareForCrossfilter();
 			initializePathViz();
 			initializeNodes();
 		}
@@ -69,6 +199,13 @@ function fetchData(){
 			throw new Error("error fetching data!");
 		}
 	});
+}
+
+function prepareForCrossfilter(data){ //fare funzionare questa roba con dati passati
+	//let parsedRoutes=parse(data); non server trasformare da stringa a array
+	console.log(data);
+	pathData=sortAndMakePath(data);
+	//return pathData
 }
 
 function appendSvg(h, w){
@@ -103,12 +240,12 @@ function xScalePointsLength(n){ //rapporto tra n dei punti (lunghezza passata) e
 }
 
 function addOnclick(){
+	//crossfilter dimension: qualcosa sulla base di cui effettuiamo un raggruppamento, per poi poter compiere operazioni (es somma o filtraggio)
 	let nodes=svg.selectAll("circle")
 	.on("click", function(d) {
-		console.log(d);
-		//estrai il nome del punto
-		//da actualData estrai tutti gli attraversamenti di quel punto (filtra actualData con quel nome)
-		//costruisci raggruppamenti del traffico per giorno/mese/anno
+		console.log("Click");
+			let name=d.fullname;
+			getPointData(name); //prendi i dati dal server	  
 	});
 }
 
@@ -119,7 +256,7 @@ return xMap;
 }
 
 function mapCoord(xScale, yScale){ //viene eseguito solo la prima volta: salva un array con le coordinate mappate a height e width dell'svg
-		mappedCoord=dataStore[1].map(function (d){
+		mappedCoord=dataStore.map(function (d){
 		let scaledCoord=[];
 		let cloned=[];
 		scaledCoord[0]=xScale(d.coord[0]);
@@ -160,7 +297,7 @@ function colorNodes(){
 
 function drawNodes(xMap, yMap){
 	let nodes=svg.selectAll("circle")
-		.data(dataStore[1]);
+		.data(dataStore);
 
 	nodes.enter()
 		.append("circle")
@@ -202,10 +339,10 @@ function yScaleMap(minY,maxY){
 }
 
 function initializeNodes(){
-	let maxX=d3.max(dataStore[1], function(d){return d.coord[0]});
-	let minX=d3.min(dataStore[1], function(d){return d.coord[0]});
-	let maxY=d3.max(dataStore[1], function(d){return d.coord[1]});
-	let minY=d3.min(dataStore[1], function(d){return d.coord[1]});
+	let maxX=d3.max(dataStore, function(d){return d.coord[0]});
+	let minX=d3.min(dataStore, function(d){return d.coord[0]});
+	let maxY=d3.max(dataStore, function(d){return d.coord[1]});
+	let minY=d3.min(dataStore, function(d){return d.coord[1]});
 
 	let xScale = xScalePoints(minX,maxX);
 	let xMap=xScaleMap(minX,maxX);
@@ -224,6 +361,12 @@ function initializeNodes(){
 }
 
 function initializePathViz(){
+	var h=800;
+	var w=800;
+	appendSvg(h,w);
+}
+
+function initializeViz(){
 	var h=800;
 	var w=800;
 	appendSvg(h,w);
@@ -311,7 +454,8 @@ function initializeCrossingViz(){
   appendSvg(h,w);
 }
 
-function parse(data){
+function parse(data){ //questo non serve: è gia un array!!
+	console.log(data);
 	let splitted=data.split(/,|\n|\r\n/); //divide a ogni "," o a nuova linea. Carriage return diverso per linux e windows
 	let cloned=[];
   for (i = 0; i < splitted.length; i=i+4) { //ricostruisce la row originaria del csv
@@ -327,37 +471,56 @@ function vehicleFilter(data){
 	return(data.filter(element => element[2]===type));
 }
 
-function sortAndMakePath(data){
+function sortById(data){
 	let sortedVehicles=data.sort(function(a,b){ //ordina l'array in base all'id del veicolo
-		if (a[1] < b[1]) {
+		if (a.id < b.id) {
     	return -1;
   	}
-  	if (a[1] > b[1]) {
+  	if (a.id > b.id) {
     	return 1;
   	}
   	return 0;
 	});
+	return sortedVehicles
+}
 
+function makePath(sortedVehicles){ //aggiungi maxDate, minDate, pathLength!
 	let arrayPath=[];
 	let singlePath=new Object();
-	singlePath.vehicleId=sortedVehicles[0][1];
-	singlePath.vehicleType=sortedVehicles[0][2];
+	singlePath.vehicleId=sortedVehicles[0].id;
+	singlePath.vehicleType=sortedVehicles[0].type;
 	singlePath.path=[];
 	let timeStamp=new Object();
-	timeStamp.time=sortedVehicles[0][0];
-	timeStamp.path=sortedVehicles[0][3];
+	timeStamp.time=sortedVehicles[0].timestamp;
+	timeStamp.path=sortedVehicles[0].gate;
 	singlePath.path.push(timeStamp);
 	for (var i=1; i<sortedVehicles.length;i++){
-		if (sortedVehicles[i][1]!==sortedVehicles[i-1][1] || (singlePath.path.length>1&&((sortedVehicles[i][3].startsWith('entrance')&&sortedVehicles[i-1][3].startsWith('entrance'))))){ //ARGH :D condizioni di terminazione: il veicolo seguente ha targa diversa oppure c'è un path di lunghezza >1 con due ingressi consecutivi
+		
+		if (sortedVehicles[i].id!==sortedVehicles[i-1].id || (singlePath.path.length>1&&((sortedVehicles[i].gate.startsWith('entrance')&&sortedVehicles[i-1].gate.startsWith('entrance'))))){ //ARGH :D condizioni di terminazione: il veicolo seguente ha targa diversa oppure c'è un path di lunghezza >1 con due ingressi consecutivi
+			/*
+			let lowest = Number.POSITIVE_INFINITY;
+			let highest = Number.NEGATIVE_INFINITY;
+			let tmp;
+			for (let i=singlePath.length-1; i>=0; i--) {
+    			tmp = singlePath[i].timeStamp;
+    			if (tmp < lowest) lowest = tmp;
+    			if (tmp > highest) highest = tmp;
+			}
+			console.log(highest, lowest);
+			singlePath.min=0;
+			singlePath.max=1;
+			*/
+			let len=singlePath.path.length;
+			singlePath.pathLength=len;
 			arrayPath.push(singlePath);
 			singlePath=new Object();
 			singlePath.path=[];
-			singlePath.vehicleId=sortedVehicles[i][1];
-			singlePath.vehicleType=sortedVehicles[i][2];
+			singlePath.vehicleId=sortedVehicles[i].id;
+			singlePath.vehicleType=sortedVehicles[i].type;
 		}
 		timeStamp=new Object();
-		timeStamp.time=sortedVehicles[i][0];
-		timeStamp.path=sortedVehicles[i][3];
+		timeStamp.time=sortedVehicles[i].timestamp;
+		timeStamp.path=sortedVehicles[i].gate;
 		singlePath.path.push(timeStamp);
 		if (i===sortedVehicles.length-1){
 			arrayPath.push(singlePath);
@@ -455,13 +618,24 @@ function drawPathCrossing(){
 	if(viewType!==2){
   	viewType=2;
   }
-  if (actualData.length===0){
-  	alert("no data to viz!");
-  	return;
-  }
   initializeCrossingViz();
   drawClickableNodes();
   addOnclick();
+}
+
+function nameFilter(name, data){
+	console.log(data);
+	console.log(name);
+	let result=[];
+	data.forEach(function(d){
+		//if (d.path[0].path===name){ //guarda se il path inizia per il punto cliccato
+			if(d.path.includes(path===name)){
+			result.push(d);
+		}
+	});
+	console.log(result);
+	//return(data.filter(element => element.path.path===name));
+
 }
 
 function lengthFilter(data){
@@ -537,3 +711,4 @@ function timeFilter(data){
 	return dayFiltered;
 }
 
+loadAll();
